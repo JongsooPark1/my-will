9d3183f0ea487739e8b42ba80d41fd90366e51dd
= <rightgear11@gmail.com>
2018-01-19 00:39:50 +0900

    initial commit

---
+++ src/main/java/com/will/MyWillApplication.java
@@ -0,0 +1,12 @@
+package com.will;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWillApplication.class, args);
+	}
+}
